c386684085c5bc4546c8d5c9dac5280e340f77cf
Alumnot2 <Alumnot2@ORD21>
2020-11-23 14:31:02 +0100

    Agregar archivos de proyecto.

---
+++ ActividadesUT4_1/ConversorBorder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ActividadesUT4_1
+{
+    public class ConversorBorder : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.Equals(true))
+            {
+                return 2;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
